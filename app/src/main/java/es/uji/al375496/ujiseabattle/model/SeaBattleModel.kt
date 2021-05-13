182e15a43e51f1ec6e4ed534cd70a059fb76f960
Diego Villabrille Seca <al375496@uji.es>
2021-05-13 19:53:53 +0200

    added states

--- app/src/main/java/es/uji/al375496/ujiseabattle/model/SeaBattleModel.kt
+++ app/src/main/java/es/uji/al375496/ujiseabattle/model/SeaBattleModel.kt
@@ -3,5 +3,6 @@ package es.uji.al375496.ujiseabattle.model
 import es.uji.al375496.ujiseabattle.model.data.Board
 
 class SeaBattleModel(private val playerBoard: Board, private val AIBoard: Board) {
-
+    var state = SeaBattleState.PLACE_SHIPS
+        private set
 }
---
+++ app/src/main/java/es/uji/al375496/ujiseabattle/model/SeaBattleState.kt
@@ -0,0 +1,11 @@
+package es.uji.al375496.ujiseabattle.model
+
+enum class SeaBattleState {
+    PLACE_SHIPS,
+    DRAG_INTO_BOARD,
+    DRAG_INSIDE_BOARD,
+    PLAYER_TURN,
+    COMPUTER_TURN,
+    WAITING,
+    END
+}
